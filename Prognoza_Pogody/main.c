729e50f432351ca2790eee911723d0646f7518e3
Piotr.Zyszczak <zychu155@wp.pl>
2016-01-07 22:19:21 +0100

    Kolejne Zabawy z bazą
    Znalazłem chyba kod ale trzeba go poprawić i ztestować

--- Prognoza_Pogody/main.c
+++ Prognoza_Pogody/main.c
@@ -8,8 +8,9 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iphlpapi.h>
-#include <stdio.h>
-
+#include <sqltypes.h>
+#include <sql.h> 
+#include <sqlext.h>
 
 #pragma comment(lib, "libws2_32.a")
 
@@ -293,8 +294,77 @@ void GenerateButtons(HWND parent, HINSTANCE hInstance){
 		parent,(HMENU)B_Option3,hInstance,NULL);
 }
 
+void show_error(unsigned int handletype, const SQLHANDLE handle)
+{
+    SQLCHAR sqlstate[1024];
+    SQLCHAR message[1024];
+    if(SQL_SUCCESS == SQLGetDiagRec(handletype, handle, 1, sqlstate, NULL, message, 1024, NULL))
+    	MessageBox(NULL, "Connection!","Message: "+message+"nSQLSTATE: "+sqlstate,MB_ICONINFORMATION|MB_OK);
+        //cout<<"Message: "<<message<<"nSQLSTATE: "<<sqlstate<<endl;
+}
+
 int FunkcjaBazodanowa(){
 	
+    SQLHANDLE sqlenvhandle;    
+    SQLHANDLE sqlconnectionhandle;
+    SQLHANDLE sqlstatementhandle;
+    SQLRETURN retcode;
+
+    if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlenvhandle))
+        goto FINISHED;
+
+    if(SQL_SUCCESS!=SQLSetEnvAttr(sqlenvhandle,SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)) 
+        goto FINISHED;
+    
+    if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_DBC, sqlenvhandle, &sqlconnectionhandle))
+        goto FINISHED;
+
+    SQLCHAR retconstring[1024];
+    switch(SQLDriverConnect (sqlconnectionhandle, 
+                NULL, 
+                (SQLCHAR*)"DRIVER={SQL Server};SERVER=oracle4.icis.pcz.pl, 1521;DATABASE=oracle4;UID=PIOZYS;PWD=Piotr66;", 
+                SQL_NTS, 
+                retconstring, 
+                1024, 
+                NULL,
+                SQL_DRIVER_NOPROMPT)){
+        case SQL_SUCCESS_WITH_INFO:
+            show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+            break;
+        case SQL_INVALID_HANDLE:
+        case SQL_ERROR:
+            show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+            goto FINISHED;
+        default:
+            break;
+    }
+    
+    if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_STMT, sqlconnectionhandle, &sqlstatementhandle))
+        goto FINISHED;
+
+    if(SQL_SUCCESS!=SQLExecDirect(sqlstatementhandle, (SQLCHAR*)"select * from testtable", SQL_NTS)){
+        show_error(SQL_HANDLE_STMT, sqlstatementhandle);
+        goto FINISHED;
+    }
+    else{
+        char name[64];
+        char address[64];
+        int id;
+        while(SQLFetch(sqlstatementhandle)==SQL_SUCCESS){
+            SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &id, 0, NULL);
+            SQLGetData(sqlstatementhandle, 2, SQL_C_CHAR, name, 64, NULL);
+            SQLGetData(sqlstatementhandle, 3, SQL_C_CHAR, address, 64, NULL);
+            MessageBox(NULL, "Connection!",id+name+address,MB_ICONINFORMATION|MB_OK);
+            //cout<<id<<" "<<name<<" "<<address<<endl;
+        }
+    }
+
+	FINISHED:
+    SQLFreeHandle(SQL_HANDLE_STMT, sqlstatementhandle );
+    SQLDisconnect(sqlconnectionhandle);
+    SQLFreeHandle(SQL_HANDLE_DBC, sqlconnectionhandle);
+    SQLFreeHandle(SQL_HANDLE_ENV, sqlenvhandle);
+    
 }
 
 LRESULT CALLBACK WndProc1(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
