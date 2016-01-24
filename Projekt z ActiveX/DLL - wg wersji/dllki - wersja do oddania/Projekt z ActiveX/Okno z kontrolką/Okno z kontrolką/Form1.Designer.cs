6fe1d9826538b7e157a28f09b8b880cb2796e655
Damian Łukasik <Damian.lukasik@mail.com>
2016-01-24 21:26:06 +0100

    Ogarnięcie projektu

---
+++ Projekt z ActiveX/DLL - wg wersji/dllki - wersja do oddania/Projekt z ActiveX/Okno z kontrolką/Okno z kontrolką/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Okno_z_kontrolką
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mojaKontrolka1 = new Projekt_ActiveX.MojaKontrolka();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kontrolka na zaliczenie z Programowania Komponentowego";
+            // 
+            // mojaKontrolka1
+            // 
+            this.mojaKontrolka1.BackColor = System.Drawing.Color.White;
+            this.mojaKontrolka1.Location = new System.Drawing.Point(78, 53);
+            this.mojaKontrolka1.Name = "mojaKontrolka1";
+            this.mojaKontrolka1.Size = new System.Drawing.Size(158, 20);
+            this.mojaKontrolka1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 151);
+            this.Controls.Add(this.mojaKontrolka1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Projekt_ActiveX.MojaKontrolka mojaKontrolka1;
+    }
+}
+
---
+++ Projekt z ActiveX/DLL - wg wersji/dllki - wersja do oddania/Projekt z ActiveX/Projekt_ActiveX/Projekt_ActiveX/IMojaKontrolka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Projekt_ActiveX
+{
+    // identyfikator globalnie unikatowy - guid , można wygenerować z Tools > Create GUID
+    //InterfaceType - typ interfejsu
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    //guidy muszą być różne
+    [Guid("713F2503-8ADB-48C6-87C6-6219029D6599")]
+    public interface IMojaKontrolka
+    {
+        void show();        
+    }
+}
